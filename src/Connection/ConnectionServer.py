c322ff0538c3c329fed4cddae5fe4f677b0e83e3
bitcrystal <bitcrystalv30@gmail.com>
2018-01-29 02:02:52 +0000

    Update ConnectionServer.py

--- src/Connection/ConnectionServer.py
+++ src/Connection/ConnectionServer.py
@@ -1,22 +1,20 @@
 import logging
 import time
 import sys
-from collections import defaultdict
 
 import gevent
 import msgpack
 from gevent.server import StreamServer
 from gevent.pool import Pool
+from collections import defaultdict
 
-import util
 from Debug import Debug
 from Connection import Connection
 from Config import config
 from Crypt import CryptConnection
 from Crypt import CryptHash
 from Tor import TorManager
-from Site import SiteManager
-
+from I2P import I2PManager
 
 class ConnectionServer:
     def __init__(self, ip=None, port=None, request_handler=None):
@@ -25,12 +23,25 @@ class ConnectionServer:
         self.last_connection_id = 1  # Connection id incrementer
         self.log = logging.getLogger("ConnServer")
         self.port_opened = None
-        self.peer_blacklist = SiteManager.peer_blacklist
+
+        config_tor = None
+        config_i2p = None
+
+        if config.tor == "disable":
+           config_tor = "disabled"
+ 
+        if config.i2p == "disable":
+           config_i2p = "disabled"
 
         if config.tor != "disabled":
             self.tor_manager = TorManager(self.ip, self.port)
         else:
             self.tor_manager = None
+
+        if config.i2p != "disabled":
+            self.i2p_manager = I2PManager(self.ip, self.port)
+        else:
+            self.i2p_manager = None
 
         self.connections = []  # Connections
         self.whitelist = config.ip_local  # No flood protection on this ips
@@ -47,8 +58,6 @@ class ConnectionServer:
         self.stat_sent = defaultdict(lambda: defaultdict(int))
         self.bytes_recv = 0
         self.bytes_sent = 0
-        self.num_recv = 0
-        self.num_sent = 0
 
         # Bittorrent style peerid
         self.peer_id = "-ZN0%s-%s" % (config.version.replace(".", ""), CryptHash.random(12, "base64"))
@@ -109,6 +118,9 @@ class ConnectionServer:
         if ip.endswith(".onion") and self.tor_manager.start_onions and site:  # Site-unique connection for Tor
             site_onion = self.tor_manager.getOnion(site.address)
             key = ip + site_onion
+        elif ip.endswith(".i2p") and self.i2p_manager.start_onions and site:  # Site-unique connection for I2P
+            site_i2p = self.i2p_manager.getOnion(site.address)
+            key = ip + site_i2p
         else:
             key = ip
 
@@ -130,6 +142,10 @@ class ConnectionServer:
                     if ip.endswith(".onion") and self.tor_manager.start_onions and ip.replace(".onion", "") != connection.target_onion:
                         # For different site
                         continue
+                    if ip.endswith(".i2p") and self.i2p_manager.start_onions and ip.replace(".i2p", "") != connection.target_i2p:
+                        # For different site
+                        continue
+
                     if not connection.connected and create:
                         succ = connection.event_connected.get()  # Wait for connection
                         if not succ:
@@ -140,13 +156,11 @@ class ConnectionServer:
         if create:  # Allow to create new connection if not found
             if port == 0:
                 raise Exception("This peer is not connectable")
-
-            if (ip, port) in self.peer_blacklist:
-                raise Exception("This peer is blacklisted")
-
             try:
                 if ip.endswith(".onion") and self.tor_manager.start_onions and site:  # Lock connection to site
-                    connection = Connection(self, ip, port, target_onion=site_onion)
+                    connection = Connection(self, ip, port, target_onion=site_onion, target_i2p=None)
+                elif ip.endswith(".i2p") and self.i2p_manager.start_onions and site:  # Lock connection to site
+                    connection = Connection(self, ip, port, target_onion=None, target_i2p=site_i2p)
                 else:
                     connection = Connection(self, ip, port)
                 self.ips[key] = connection
@@ -159,10 +173,6 @@ class ConnectionServer:
             except Exception, err:
                 connection.close("%s Connect error: %s" % (ip, Debug.formatException(err)))
                 raise err
-
-            if len(self.connections) > config.global_connected_limit:
-                gevent.spawn(self.checkMaxConnections)
-
             return connection
         else:
             return None
@@ -175,6 +185,10 @@ class ConnectionServer:
         if connection.target_onion:
             if self.ips.get(connection.ip + connection.target_onion) == connection:
                 del self.ips[connection.ip + connection.target_onion]
+        elif connection.target_i2p:
+            if self.ips.get(connection.ip + connection.target_i2p) == connection:
+                del self.ips[connection.ip + connection.target_i2p]
+
         # Cert pinned connection
         if connection.cert_pin and self.ips.get(connection.ip + "#" + connection.cert_pin) == connection:
             del self.ips[connection.ip + "#" + connection.cert_pin]
@@ -192,11 +206,6 @@ class ConnectionServer:
             last_message_time = 0
             s = time.time()
             for connection in self.connections[:]:  # Make a copy
-                if connection.ip.endswith(".onion"):
-                    timeout_multipler = 2
-                else:
-                    timeout_multipler = 1
-
                 idle = time.time() - max(connection.last_recv_time, connection.start_time, connection.last_message_time)
                 last_message_time = max(last_message_time, connection.last_message_time)
 
@@ -217,16 +226,16 @@ class ConnectionServer:
                     if not connection.ping():
                         connection.close("[Cleanup] Ping timeout")
 
-                elif idle > 10 * timeout_multipler and connection.incomplete_buff_recv > 0:
+                elif idle > 10 and connection.incomplete_buff_recv > 0:
                     # Incomplete data with more than 10 sec idle
                     connection.close("[Cleanup] Connection buff stalled")
 
-                elif idle > 10 * timeout_multipler and connection.protocol == "?":  # No connection after 10 sec
+                elif idle > 10 and connection.protocol == "?":  # No connection after 10 sec
                     connection.close(
                         "[Cleanup] Connect timeout: %.3fs" % idle
                     )
 
-                elif idle > 10 * timeout_multipler and connection.waiting_requests and time.time() - connection.last_send_time > 10 * timeout_multipler:
+                elif idle > 10 and connection.waiting_requests and time.time() - connection.last_send_time > 10:
                     # Sent command and no response in 10 sec
                     connection.close(
                         "[Cleanup] Command %s timeout: %.3fs" % (connection.last_cmd_sent, time.time() - connection.last_send_time)
@@ -237,7 +246,7 @@ class ConnectionServer:
                         "[Cleanup] Too many bad actions: %s" % connection.bad_actions
                     )
 
-                elif idle > 5 * 60 and connection.sites == 0:
+                elif idle > 5*60 and connection.sites == 0:
                     connection.close(
                         "[Cleanup] No site for connection"
                     )
@@ -247,7 +256,7 @@ class ConnectionServer:
                     connection.bad_actions = 0
 
             # Internet outage detection
-            if time.time() - last_message_time > max(60, 60 * 10 / max(1, float(len(self.connections)) / 50)):
+            if time.time() - last_message_time > max(60, 60*10/max(1,float(len(self.connections))/50)):
                 # Offline: Last message more than 60-600sec depending on connection number
                 if self.has_internet:
                     self.has_internet = False
@@ -260,28 +269,6 @@ class ConnectionServer:
 
             if time.time() - s > 0.01:
                 self.log.debug("Connection cleanup in %.3fs" % (time.time() - s))
-
-    @util.Noparallel(blocking=False)
-    def checkMaxConnections(self):
-        if len(self.connections) < config.global_connected_limit:
-            return 0
-
-        s = time.time()
-        num_connected_before = len(self.connections)
-        self.connections.sort(key=lambda connection: connection.sites)
-        num_closed = 0
-        for connection in self.connections:
-            idle = time.time() - max(connection.last_recv_time, connection.start_time, connection.last_message_time)
-            if idle > 60:
-                connection.close("Connection limit reached")
-                num_closed += 1
-            if num_closed > config.global_connected_limit * 0.1:
-                break
-
-        self.log.debug("Closed %s connections of %s after reached limit %s in %.3fs" % (
-            num_closed, num_connected_before, config.global_connected_limit, time.time() - s
-        ))
-        return num_closed
 
     def onInternetOnline(self):
         self.log.info("Internet online")
